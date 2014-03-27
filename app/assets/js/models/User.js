c6aa1f1b1c297f353cb9f690f596c13c891851ae
Dale Corns <drcorns@hotmail.com>
2014-03-27 07:30:24 -0700

    Add BB user model

---
+++ app/assets/js/models/User.js
@@ -0,0 +1,13 @@
+'use strict';
+var Backbone = require('backbone');
+
+module.exports = Backbone.Model.extend({
+  idAttribute: '_id',
+  urlRoot: 'http://localhost:3000/somewhere/users',
+  defaults: {
+    first_name: '',
+    last_name: '',
+    email: '',
+    username:''
+  }
+});
