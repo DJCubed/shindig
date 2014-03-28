9fd94e8ee8cbe700d25b44e4ead75b1427ea76ef
Dale Corns <drcorns@hotmail.com>
2014-03-28 14:01:54 -0700

    add version v1 to BB models

--- app/assets/js/models/User.js
+++ app/assets/js/models/User.js
@@ -3,7 +3,7 @@ var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
   idAttribute: '_id',
-  urlRoot: 'http://localhost:3000/somewhere/users',
+  urlRoot: 'http://localhost:3000/v1/users',
   defaults: {
     first_name: '',
     last_name: '',
