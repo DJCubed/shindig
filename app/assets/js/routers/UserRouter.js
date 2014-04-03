03fd32e9abec6940eac17625e1bfe2d48215ef95
JamesDLorence <jamesdlorence@gmail.com>
2014-04-02 18:12:39 -0700

    Broken RegisterView, still working on it

--- app/assets/js/routers/UserRouter.js
+++ app/assets/js/routers/UserRouter.js
@@ -6,8 +6,14 @@ var UserCollection     = require('../models/UserCollection');
 var UserCollectionView = require('../views/UserCollectionView');
 
 module.exports = Backbone.Router.extend({
-  routes: {'users/:id': 'show',
+  routes: {'register': 'create',
+           'users/:id': 'show',
            'users': 'index'},
+
+  create: function() {
+    var registerForm = new RegisterForm({model: new User});
+    $('.mainContent').replaceWith(registerForm.el);
+  },
 
   show: function(id) {
     console.log(id);
